f2f9a7a24460a80d15b7378cfd302e011f69c225
Clara.Draper-NOAA <clara.draper@noaa.gov>
2026-05-15 20:02:48 +0000

    Add a warning to crash if don't have reject station lists as dict.

--- src/jcb/observation_chronicle/observation_chronicle.py
+++ src/jcb/observation_chronicle/observation_chronicle.py
@@ -83,6 +83,8 @@ class ObservationChronicle():
         if obs_chronicle['observer_type'] == 'conventional':
             # need to check the use of individual stations/locations
             stations_to_reject = obs_chronicle.get('stations_to_reject', {})
+            jcb.abort_if(not isinstance(stations_to_reject, dict),
+                       f"stations_to_reject must be a dictionary.")
             variables = [name for name in stations_to_reject.keys() if name != 'All']
 
             if not any(self.get_conventional_rejected_stations(observer, variable)
